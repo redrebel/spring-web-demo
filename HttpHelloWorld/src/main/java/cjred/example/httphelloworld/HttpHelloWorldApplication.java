6db27f2ed7e364d1ac45c53696dc0c54573e0c11
Lee, Jung Geun <cjred77@gmail.com>
2019-08-16 21:37:17 +0900

    add example

---
+++ HttpHelloWorld/src/main/java/cjred/example/httphelloworld/HttpHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package cjred.example.httphelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpHelloWorldApplication.class, args);
+    }
+
+}
